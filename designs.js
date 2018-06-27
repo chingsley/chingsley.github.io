a113d9c153bcb01377afd08d87152781b0f07046
chingsley <chingsleychinonso@gmail.com>
2018-06-28 00:44:21 +0100

    PixelArtMaker fourth commit, modified the look of nav bar

--- designs.js
+++ designs.js
@@ -27,18 +27,33 @@
 }
 
 {// SET BACKGROUND COLOR
-    let bg_color = $('#bg_colorPicker').val();
+    var bg_color = $('#bg_colorPicker').val();
     $('#bg_colorPicker').change(function(){
         bg_color = $(this).val();
         $('#pixelCanvas').css('background-color', bg_color);
-    });
-  
+        $('.cell').css('background-color', bg_color);
+    }); 
 }
+
+{//RGB TO HEX CONVERSION FUCNTIONS
+    function component_to_hex(c){
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex: hex;
+    }
+    function rgb_to_hex(r, g, b){
+        return "#"+ component_to_hex(r) + component_to_hex(g) + component_to_hex(b);
+    }
+    console.log(rgb_to_hex(0, 51, 255));
+    
+}
+
 
 {// APPLY COLOR TO CELL WHEN CLICKED
     $('#pixelCanvas').on('click','.cell',function(evt){
         $(evt.target).css('background-color', color);
-        console.log($( evt.target ).css('background-color',color));
+        console.log($(this).css('background-color'));
+        
+      
     });
 }
 
@@ -47,4 +62,12 @@
         $("#submit_form").trigger('click');
     });
 }
+
+
+/**
+ * ================ FUNCTION TO STUDY =======================
+ * var rgb = $(this).css('background-color');
+        var cell_hex_color = '#' + rgb.substr(4, rgb.indexOf(')') - 4).split(',').map((color) => parseInt(color).toString(16)).join('');
+       
+ */
 
